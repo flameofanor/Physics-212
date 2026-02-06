807a213f7ea1d6b649c5d8ffd64e32f86d9559fa
Will Cash <willcasholson@gmail.com>
2026-02-05 19:45:01 -0900

    gg

--- Homework3/Problem3.py
+++ Homework3/Problem3.py
@@ -3,8 +3,8 @@ from math import sin, cos, tan, asin, acos, atan
 from math import pow, sqrt
 from math import pi, e
 
-# f"{some_float:.4f}"
-# print("x = " + f"{x:.4f}" + " \u03A9")
+# f"{some_float:.2f}"
+# print("R1 = " + f"{r1:.2f}" + " \u03A9")
 
 #remember that trig functions are all in radians
 
@@ -27,3 +27,22 @@ deltaT =
 '''
 
 #problem variables
+
+q1 = 5.11e-9
+q2 = -5.11e-9
+q3 = 17.2e-9
+q4 = -16.3e-9
+
+Flux_1 = q1 / E0
+print("Flux 1 = " + f"{Flux_1:.4f}")
+
+Flux_2 = (q1 + q2) / E0
+print("Flux 2 = " + f"{Flux_2:.4f}")
+
+Flux_3 = (q2 + q3) / E0
+print("Flux 3 = " + f"{Flux_3:.4f}")
+
+Flux_4 = (q1 + q2 + q3 + q4) / E0
+print("Flux 4 = " + f"{Flux_4:.4f}")
+
+#nice!
